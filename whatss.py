a73b1336081cdc4b89afe3e9172fb15f69a60de7
mohamed <mohamed99elsokary@gmail.com>
2022-02-03 19:47:09 +0200

    update

--- whatss.py
+++ whatss.py
@@ -1,16 +1,16 @@
 import webbrowser
 import pyautogui as gui
 import keyboard
-import time 
+import time
 import string
-import random
-#---------------------new numbers------------------#
+
+# ---------------------new numbers------------------#
 new_numbers = input("new numbers : ")
 file_name = input("name the output file : ")
 
-#---------------handel the new numbers--------------#
+# ---------------handel the new numbers--------------#
 for x in string.ascii_letters:
-    new_numbers = new_numbers.replace(x+",", "")
+    new_numbers = new_numbers.replace(x + ",", "")
     new_numbers = new_numbers.replace(x, "")
 new_numbers = new_numbers.replace("+", "'+")
 new_numbers = new_numbers.replace(",", "',")
@@ -38,50 +38,58 @@ for x in range(11):
     new_numbers = new_numbers.replace("'06", "'")
     new_numbers = new_numbers.replace("'07", "'")
     new_numbers = new_numbers.replace("'08", "'")
-    new_numbers = new_numbers.replace("'09", "'")    
-    new_numbers = new_numbers.replace("'',", "")    
+    new_numbers = new_numbers.replace("'09", "'")
+    new_numbers = new_numbers.replace("'',", "")
 new_numbers = new_numbers.replace("'", "")
-new_numbers = new_numbers.replace(",", '''
-''')
-#----------------------write-------------------#
-with open(file_name + '.txt', 'a') as file:
+new_numbers = new_numbers.replace(
+    ",",
+    """
+""",
+)
+# ----------------------write-------------------#
+with open(file_name + ".txt", "a") as file:
     print()
-with open(file_name + '.txt', 'r+') as file:
+with open(file_name + ".txt", "r+") as file:
     for line in file:
         if new_numbers in line:
-           break
-    else:    
+            break
+    else:
         file.write(new_numbers + "\n")
-#target_place = input ("target place : ")
+# target_place = input ("target place : ")
 target_place = file_name
-limite = input ("limite the usage : ")
+limite = input("limite the usage : ")
 check = limite.isdigit()
-if ((check == False) and (limite != "unlimted")):
+if (check == False) and (limite != "unlimted"):
     exit()
-message = input ("message : ")
-#-------------------read------------------------#
-with open(target_place + '.txt') as f:
+message = input("message : ")
+# -------------------read------------------------#
+with open(target_place + ".txt") as f:
     number = [line.rstrip() for line in f]
-#-------------------run------------------------#    
-sent_msgs=0
+# -------------------run------------------------#
+sent_msgs = 0
 while True:
-    if (str(sent_msgs) >= limite  and limite != "unlimted"):
-        print ("done successfully")
-        exit()    
-    #number[sent_msgs] = '01119727679' #spam for one number
-    url = 'https://web.whatsapp.com/send?phone=%2B2'+ number[sent_msgs] +'&text&app_absent=0'
-    print (number[sent_msgs])
-#----------------------------------------
+    if str(sent_msgs) >= limite and limite != "unlimted":
+        print("done successfully")
+        exit()
+    # number[sent_msgs] = '01119727679' #spam for one number
+    url = (
+        "https://web.whatsapp.com/send?phone=%2B2"
+        + number[sent_msgs]
+        + "&text&app_absent=0"
+    )
+    print(number[sent_msgs])
+    # ----------------------------------------
     webbrowser.open(url)
-    time.sleep(5)
-    #gui.click(chatposition)#for now mish bnst8dmo
+    time.sleep(6)
+    chatposition = (2850, 1028)
+    gui.click(chatposition)  # for now mish bnst8dmo
     keyboard.write(message)
-    gui.press('enter')
-    time.sleep(1)
-    sent_msgs = sent_msgs + 1    
-    print (sent_msgs)
-    time.sleep(1)
-    keyboard.press_and_release('ctrl+w')
-    gui.press('enter')
-#------------------------- mkan el mouse ---------------#
-#print (gui.position())
+    print("tba3")
+    gui.press("enter")
+    time.sleep(2)
+    sent_msgs = sent_msgs + 1
+    keyboard.press_and_release("ctrl+w")
+# ------------------------- mkan el mouse ---------------#
+
+# while True:
+#     print(gui.position())
